9766a2514f9879b93a63756608e48a25c734986a
Akshat Gupta <aakshat11g@gmail.com>
2026-09-03 00:38:53 +0530

    Auth System + Routes

--- MINI_TASK1/Backend/src/app.js
+++ MINI_TASK1/Backend/src/app.js
@@ -1,7 +1,10 @@
 import express from 'express';
+import router from './routes/auth.route.js';
 
 const app = express();
 
 app.use(express.json());
+
+app.use("/api/auth", router);
 
 export default app;
